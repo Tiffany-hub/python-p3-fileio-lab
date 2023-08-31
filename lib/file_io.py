be058661ec1e6e45a1443e44ca3e9e3a96329cc0
+ <+>
2023-09-01 01:12:56 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as f :
+        f.write(file_content)
+        
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    write_file(file_name = "logfile", file_content = "Log 1: 5 bananas added")
+    append_file(file_name="logfile", append_content="\nLog 2: 3 bananas subtracted")
+    content = read_file(file_name = "logfile")
+    print(content)
